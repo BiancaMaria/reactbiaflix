ad229e5ec65a1ba5ce2fb4da5428a9d6e3a1076c
Bianca Maria <bianca.engcomp@gmail.com>
2020-08-03 16:17:48 -0300

    Atualizações baseadas na aula 03

---
+++ src/components/FormField/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function FormField({label,type,name,value,onChange}){
+
+    if(type === "textArea"){
+        return(
+            <div>
+                <label>
+                    {label}:
+                <textArea
+                    type={type}
+                    value={value}
+                    name={name}
+                    onChange = {onChange}          
+                />
+                </label>
+           </div>
+        )
+    }else{
+        return(
+            <div>
+                <label>
+                    {label}:
+                <input
+                    type={type}
+                    value={value}
+                    name={name}
+                    onChange = {onChange}          
+                />
+                </label>
+           </div>
+        )
+    }
+    
+}
+
+export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,109 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
-
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            <form>
+    const valoresIniciais = {
+        nome:'',
+        descricao:'',
+        cor:'',
+    }
+    const [categorias, setCategorias] = useState(['']);
+    const [values, setValues] =  useState(valoresIniciais);
+
+    function setValue(chave,valor){
+        setValues({
+            ...values,
+            [chave]:valor,//nome:'valor'
+        })
+    }
+
+    function handleChange(infordoEvento){
+      
+        setValue(
+            infordoEvento.target.getAttribute('name'),
+            infordoEvento.target.value
+        );
+    }
+
+    return( 
+        <PageDefault>{/*qualquer coisa botar {values.nome} dentro do H1*/}
+        <h1>Cadastro de Categoria: </h1>
+
+        <form onSubmit={function handleSubmit(infordoEvento){
+            infordoEvento.preventDefault();
+            setCategorias([
+                ...categorias,
+                values
+            ]);
+
+            setValues(valoresIniciais)
+            }}>
+                
+        <FormField
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={values.nome}
+            onChange = {handleChange}
+        />
+
+        <FormField
+            
+            label="Descrição"
+            type="textArea"
+            name="descricao"
+            value={values.descricao}
+            onChange = {handleChange} 
+        />    
+        {/*<div>            
+            <label>
+            Descrição:
+            <textarea
+                type="text"
+                value={values.descricao}
+                name="descricao"
+                onChange = {handleChange}
+            />
+            </label>
+        </div>*/}
+
+        <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange = {handleChange}
+        />
+
+            {/*<div>            
                 <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                    />
+                Cor:
+                <input
+                    type="color"
+                    value={values.cor}
+                    name="cor"
+                    onChange = {handleChange}          
+                />
                 </label>
+            </div>*/}
 
-                <button>
-                    Cadastrar
-                </button>
-            </form>
 
+            <button>
+                Cadastrar
+            </button>
+        </form>
+
+        <ul style={{listStyle: 'none'}}>
+           {categorias.map((categoria, indice) => {
+               return(
+               <li key={`${categoria}${indice}`}>
+                   {categoria.nome}
+                </li>
+               )
+           })}             
+        </ul>                
 
         <Link to="/">Ir para home</Link>
         </PageDefault>
